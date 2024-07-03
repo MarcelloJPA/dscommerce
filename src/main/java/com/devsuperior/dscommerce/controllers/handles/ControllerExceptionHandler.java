ffd8da09385920d35040f611b39d9919eb98c46e
Marcello <cellolim4jpa@gmail.com>
2024-07-03 10:49:21 -0300

    Bean Validation

--- src/main/java/com/devsuperior/dscommerce/controllers/handles/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/handles/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dscommerce.controllers.handles;
 
 import com.devsuperior.dscommerce.dto.CustomError;
+import com.devsuperior.dscommerce.dto.ValidationError;
 import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,4 +29,19 @@ public class ControllerExceptionHandler {
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        /*Nesse caso foi a ValidationError, pois eu consigo incluir na lista os erros que ocorreram e retornar na tela*/
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados Inválidos", request.getRequestURI());
+        
+        for(FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());  /*O campo que deu erro e qual foi o erro*/
+        }
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+
 }
